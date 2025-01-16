733a6cc6410fb794822377aaec40612f95f3261e
linhtran <linh.tran@saleshood.com>
2025-01-16 10:06:23 +0700

    chore: config project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths  from "vite-tsconfig-paths";
+import dynamicPrefetch from 'vite-plugin-dynamic-prefetch';
+// import dynamicPrefetch from './vite-prefetchPlugin'
+import path from "path";
+ 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), 
+    tsConfigPaths({configNames: ['tsconfig.app.json']}),
+    dynamicPrefetch()],
+  base: 'public',
+  resolve: {
+    alias:[
+      { find: 'pages', replacement: path.resolve(__dirname,'src/pages/' )},
+      { find: /^components/, replacement: path.resolve(__dirname,'src/components') },
+      { find: 'utils', replacement: path.resolve(__dirname,'src/utils') }
+    ]
+  },
+  build: {
+    minify: true,
+    sourcemap: true,
+    outDir: "dist",
+    rollupOptions: {
+      plugins: [],
+      // external: ['fs', 'path', 'os', 'crypto']
+    },
+  },
+});
